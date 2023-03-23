9c7c421ffd93f4791cf7dcfeb6c2893b9c091e47
OdeToCode147 <vamsirao007@gmail.com>
2023-03-23 23:52:18 +0530

    first Commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+
+    <div className='header'>
+        <img src='https://icon-library.com/images/mail-icon-black-and-white/mail-icon-black-and-white-6.jpg' alt='Logo' width="60px"/>
+        <span className='title'>OutLook Mail</span>
+    </div>
+  )
+}
+
+export default Header
